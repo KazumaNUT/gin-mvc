ee99851fc2e0067b40ae4a7d9f2c4426feb6e6db
Sato <satoma.2001.0920@gmail.com>
2022-08-11 13:08:48 +0900

    プロトタイプ作成

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"gin-mvc/controller"
 )
 
 func main() {
-	engine := gin.Default()
-	engine.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
-	engine.Run(":3000")
+	router := controller.GetRouter()
+	router.Run(":8080")
 }
